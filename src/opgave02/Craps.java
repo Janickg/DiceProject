f12f777bf3047f3d4137a8202a310982e13ef767
Janickg <janickg1@live.dk>
2024-09-20 12:01:17 +0200

    Opgaver 5.0 (final edition)

--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class Craps {
 
     private static int point = 0;//felt variabel der gemmer summen fra første kast og bruger den i continueGame metode
+    private static int wins = 0;
+    private static int losses = 0;
 
     public static void main(String[] args) {
-        playCrabs(rollDice()); //metode der starter spillet
+        playCrabs(); //metode der starter spillet
+        printStats();
     }
 
     public static int[] rollDice() {
@@ -17,22 +20,40 @@ public class Craps {
         return rolls;
     }
 
-    public static void playCrabs(int[] rolls) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Tryk enter for at starte, nej for at stoppe. ");
-        String answer = scanner.nextLine(); //scanner der registrer user input for at slå terning
+    public static void playCrabs() {
+        String answer1 = playAgain();
 
+        while (!answer1.equals("nej")) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Tryk enter for at starte");
+            String answer = scanner.nextLine(); //scanner der registrer user input for at slå terning
 
-        int rollsSum = rolls[0] + rolls[1];
-        if (rollsSum == 7 || rollsSum == 11) { //Win condition på første slag
-            System.out.println("Du slog: " + rolls[0] + " og " + rolls[1] + " = " + rollsSum + ", du vandt!");
-        } else if (rollsSum == 2 || rollsSum == 3 || rollsSum == 12) { //lose condtion på første slag
-            System.out.println("Du slog: " + rolls[0] + " og " + rolls[1] + " = " + rollsSum + ", du tabte!");
-        } else {
-            point = rollsSum;
-            System.out.println("Du slog " + rolls[0] + " og " + rolls[1] + ", du skal nu ramme " + rollsSum + " point.");
-            if (!continuegame(point)) ;
-        } //metode for at fortsætte spillet hvis hverken win eller lose condition opfyldes retunere false hvis spillet skal stoppes
+            int[] rolls1 = rollDice();
+
+            int rollsSum = rolls1[0] + rolls1[1];
+            if (rollsSum == 7 || rollsSum == 11) { //Win condition på første slag
+                System.out.println("Du slog: " + rolls1[0] + " og " + rolls1[1] + " = " + rollsSum + ", du vandt!");
+                answer1 = playAgain();
+                wins++;
+            } else if (rollsSum == 2 || rollsSum == 3 || rollsSum == 12) { //lose condtion på første slag
+                System.out.println("Du slog: " + rolls1[0] + " og " + rolls1[1] + " = " + rollsSum + ", du tabte!");
+                answer1 = playAgain();
+                losses++;
+            } else {
+                point = rollsSum;
+                System.out.println("Du slog " + rolls1[0] + " og " + rolls1[1] + ", du skal nu ramme " + rollsSum + " point.");
+                if (!continuegame(point)) {
+                    answer1 = playAgain();
+                }
+            }//metode for at fortsætte spillet hvis hverken win eller lose condition opfyldes retunere false hvis spillet skal stoppes
+        }
+    }
+
+    public static String playAgain () {
+        System.out.println("Vil du spille igen? (ja/nej)");
+        Scanner input = new Scanner(System.in);
+        String answer1 = input.nextLine();
+        return answer1;
     }
 
     public static boolean continuegame(int point) { //metode for at fortsætte spillet
@@ -49,14 +70,21 @@ public class Craps {
 
             if (sum == point) { //win condtioin
                 System.out.println("Du slog " + faces[0] + " og " + faces[1] + " = " + sum + ", du har vundet! ");
+                wins++;
                 return false;
             } else if (sum == 7) {//lose condition
                 System.out.println("Du slog " + faces[0] + " og " + faces[1] + " = " + sum + ", du har tabt! ");
+                losses++;
                 return false;
             }
             System.out.println("Tryk enter for at rulle. ");
             answer = scan.nextLine();
         }
         return true;
+    }
+
+    public static void printStats() {
+        System.out.println("Antal wins: " + wins);
+        System.out.println("Antal losses: " + losses);
     }
 }
